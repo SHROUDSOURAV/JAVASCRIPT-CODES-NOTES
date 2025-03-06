885a14ff9621041c8364127a471fbe57b97f815f
SOURAV BOSE <souravbose361@gmail.com>
2025-03-06 20:57:44 +0530

    conversion operations in js

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,25 @@
+let num = 358;  //integer type variable
+
+let num2 = String(num);     //converting integer variable to String type
+
+console.log(typeof(num));
+console.log(typeof(num2));
+
+// ****************Operations**********************
+console.log(2+2);   //addition
+console.log(2-2);   //subtraction
+console.log(3*2);   //multiplication
+console.log(3**3);  //power
+console.log(15/3);  //quotient
+console.log(15%4);  //remainder
+
+//Concatenating String
+let var1 = "sourav";
+let var2 = "bose";
+let var3 = var1+" "+var2;
+console.log(var3);
+
+let v1 = 10;
+let v2 = 20;
+console.log(v1++);  //post increment
+console.log(++v2);  //pre increment
